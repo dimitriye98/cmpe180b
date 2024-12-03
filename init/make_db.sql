03d9b312a6d6bf9aee9f15d8910826e58addd82c
Stephen Lee <stephenabcba@gmail.com>
2024-12-02 16:56:22 -0800

    update database schema

--- init/make_db.sql
+++ init/make_db.sql
@@ -26,31 +26,14 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `SalesDB`.`Acquisition_Prices`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `SalesDB`.`Acquisition_Prices` (
-  `SKU` INT NOT NULL,
-  `Start_Time` DATETIME NOT NULL,
-  `Price` DOUBLE(20,2) NOT NULL,
-  INDEX `fk_Acquisition_Prices_Products1_idx` (`SKU` ASC) VISIBLE,
-  PRIMARY KEY (`SKU`, `Start_Time`),
-  CONSTRAINT `fk_Acquisition_Prices_Products1`
-    FOREIGN KEY (`SKU`)
-    REFERENCES `SalesDB`.`Products` (`SKU`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `SalesDB`.`Selling_Prices`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `SalesDB`.`Selling_Prices` (
   `SKU` INT NOT NULL,
-  `Start_TIme` DATETIME NOT NULL,
+  `Start_Time` DATETIME NOT NULL,
   `Price` DOUBLE(20,2) NOT NULL,
   INDEX `fk_Selling_Prices_Products1_idx` (`SKU` ASC) VISIBLE,
-  PRIMARY KEY (`SKU`, `Start_TIme`),
+  PRIMARY KEY (`SKU`, `Start_Time`),
   CONSTRAINT `fk_Selling_Prices_Products1`
     FOREIGN KEY (`SKU`)
     REFERENCES `SalesDB`.`Products` (`SKU`)
@@ -60,19 +43,117 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `SalesDB`.`Transactions`
+-- Table `SalesDB`.`PurchaseTransactions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `SalesDB`.`Transactions` (
+CREATE TABLE IF NOT EXISTS `SalesDB`.`PurchaseTransactions` (
   `Transaction_ID` INT NOT NULL,
   `SKU` INT NOT NULL,
   `Volume` INT NOT NULL,
   `Unit_Price` DOUBLE(20,2) NOT NULL,
-  `Transaction_Time` DATETIME NOT NULL,
   PRIMARY KEY (`Transaction_ID`, `SKU`),
   INDEX `fk_Transactions_Products1_idx` (`SKU` ASC) VISIBLE,
   CONSTRAINT `fk_Transactions_Products1`
     FOREIGN KEY (`SKU`)
     REFERENCES `SalesDB`.`Products` (`SKU`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SalesDB`.`Inventories`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SalesDB`.`Inventories` (
+  `SKU` INT NOT NULL,
+  `Volume` INT NOT NULL,
+  PRIMARY KEY (`SKU`),
+  CONSTRAINT `fk_Inventory_Products1`
+    FOREIGN KEY (`SKU`)
+    REFERENCES `SalesDB`.`Products` (`SKU`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SalesDB`.`SalesTransactions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SalesDB`.`SalesTransactions` (
+  `Transaction_ID` INT NOT NULL,
+  `SKU` INT NOT NULL,
+  `Volume` INT NOT NULL,
+  `Unit_Price` DOUBLE(20,2) NOT NULL,
+  PRIMARY KEY (`Transaction_ID`, `SKU`),
+  INDEX `fk_Transactions_Products1_idx` (`SKU` ASC) VISIBLE,
+  CONSTRAINT `fk_Transactions_Products10`
+    FOREIGN KEY (`SKU`)
+    REFERENCES `SalesDB`.`Products` (`SKU`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SalesDB`.`Customers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SalesDB`.`Customers` (
+  `Customer_ID` INT NOT NULL,
+  `Name` VARCHAR(45) NULL,
+  `Address` VARCHAR(255) NULL,
+  PRIMARY KEY (`Customer_ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SalesDB`.`SalesTransactionDetails`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SalesDB`.`SalesTransactionDetails` (
+  `Transaction_ID` INT NOT NULL,
+  `Customer_ID` INT NOT NULL,
+  `Transaction_Time` DATETIME NOT NULL,
+  PRIMARY KEY (`Transaction_ID`),
+  INDEX `fk_SalesTransactionDetails_Customers1_idx` (`Customer_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_SalesTransactionDetails_SalesTransactions1`
+    FOREIGN KEY (`Transaction_ID`)
+    REFERENCES `SalesDB`.`SalesTransactions` (`Transaction_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_SalesTransactionDetails_Customers1`
+    FOREIGN KEY (`Customer_ID`)
+    REFERENCES `SalesDB`.`Customers` (`Customer_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SalesDB`.`Vendors`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SalesDB`.`Vendors` (
+  `Vendor_ID` INT NOT NULL,
+  `Name` VARCHAR(45) NULL,
+  `Address` VARCHAR(255) NULL,
+  PRIMARY KEY (`Vendor_ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SalesDB`.`PurchaseTransactionDetails`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `SalesDB`.`PurchaseTransactionDetails` (
+  `Transaction_ID` INT NOT NULL,
+  `Vendor_ID` INT NOT NULL,
+  `Transaction_Time` DATETIME NOT NULL,
+  PRIMARY KEY (`Transaction_ID`),
+  INDEX `fk_PurchaseTransactionDetails_Vendors1_idx` (`Vendor_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_PurchaseTransactionDetails_PurchaseTransactions1`
+    FOREIGN KEY (`Transaction_ID`)
+    REFERENCES `SalesDB`.`PurchaseTransactions` (`Transaction_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PurchaseTransactionDetails_Vendors1`
+    FOREIGN KEY (`Vendor_ID`)
+    REFERENCES `SalesDB`.`Vendors` (`Vendor_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
